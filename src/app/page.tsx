12e187e26c18baf1dec6800f0d3289b8b63fac2b
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-12-12 17:03:14 +0900

    강아지 사진 url 넣기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import Image from 'next/image'
 import Head from "next/head";
+import DogsListView from "@/components/DogsListView";
 
 export const metadata = {
   title: 'Contact',
   description: 'description for the contact page',
 }
+const imageUrls= [
+  '/images/dog1.jpg',
+  '/images/dog2.jpg',
+  '/images/dog3.jpg',
+  '/images/dog4.jpg',
+  '/images/dog5.jpg',
+  '/images/dog6.jpg',
+  '/images/dog7.jpg',
+  '/images/dog8.jpg',
+  '/images/dog9.jpg',
+  '/images/dog10.png',
+  '/images/dog11.png',
+  '/images/dog12.jpg',
+]
 export default function Home() {
   return (
     <div className='mt-[200px]'>
       <p className='font-bold text-center uppercase text-3xl'>contact</p>
+      <DogsListView images={imageUrls} />
     </div>
   )
 }

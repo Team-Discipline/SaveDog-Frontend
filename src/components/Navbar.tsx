d41d49f81a9ee3c8d49ed80ac6a94887f917f1cc
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-12-14 01:54:01 +0900

    navbar 링크 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,11 +39,10 @@ export const Navbar = () => {
             </div>
           </div>
           <div className='flex items-center space-x-1'>
-            <Link href='#' className='py-5 px-3'>
+            <Link href='/login' className='py-5 px-3'>
               Login
             </Link>
-            <Link href='#' className='py-2 px-3 bg-yellow-400 hover:bg-yellow-
-              300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300'>
+            <Link href='/signup' className='py-2 px-3 bg-gray hover: text-gray'>
               Signup
             </Link>
           </div>

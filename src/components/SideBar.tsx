c20255a59268f0d9e9ee1dc77fe06c66ddd776a8
junghanlee <dlwjdgks126@hansung.ac.kr>
2024-02-18 18:36:01 +0900

    확장 여부에 따른 스트리머 정보 출력

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -26,25 +26,85 @@ export const SideBar = ({serverSideData}: any) => {
     // 사이드바 테스트용 더미데이터
     const streamerInfo = [
       {
-        streamerName: 'rlgus1006',
-        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/86edb3b8-4218-4125-827a-b93d754a00ee-profile_image-70x70.png'
+        streamerId: 'airikanna_stellive',
+        streamerName: '아이리_칸나',
+        profileImage: "https://static-cdn.jtvnw.net/jtv_user_pictures/57ee5d17-c96e-4046-bc00-1eab91bb9466-profile_image-70x70.png",
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '1063'
       },
       {
-        streamerName: 'nodolly',
-        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/267e8677-6d58-45ad-b64d-61400298d935-profile_image-70x70.png'
+        streamerId: 'rkdwl12',
+        streamerName:'강지',
+        profileImage: "https://static-cdn.jtvnw.net/jtv_user_pictures/fe75f0b7-a0a2-4dc0-bde1-edcb7a4fd32e-profile_image-70x70.png",
+        game: 'Just Chatting',
+        isLive: '방송 중',
+        viewers: '684',
       },
       {
-        streamerName: 'kss7749',
-        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/835fb20f-cfbb-44f4-bfe1-ba3ad23a8046-profile_image-70x70.png'
+        streamerId: 'handongsuk',
+        streamerName: '한동숙',
+        profileImage: "https://static-cdn.jtvnw.net/jtv_user_pictures/c5a2baa2-74ed-4b72-b047-8326572c9bfa-profile_image-70x70.png",
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '2172',
       },
       {
-        streamerName: 'mawang0216',
-        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/2eb00bd6-38c8-48c7-aea6-7d20e12ca383-profile_image-70x70.png'
+        streamerId: 'tmxk319',
+        streamerName: '괴물쥐123',
+        profileImage: "https://static-cdn.jtvnw.net/jtv_user_pictures/59b10cc7-772e-48a9-92df-066e9a35862b-profile_image-70x70.jpg",
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '3368',
       },
       {
-        streamerName: 'pikra10',
-        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/f1927e40-6561-4af3-970d-4711df08bb52-profile_image-70x70.jpg'
-      }
+        streamerId: 'arahashitabi_stellive',
+        streamerName: '아라하시_타비',
+        profileImage: "https://static-cdn.jtvnw.net/jtv_user_pictures/1489b16c-5e9e-4c95-8339-f4abd273f8a8-profile_image-70x70.png",
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '800',
+      },
+      {
+        streamerId: 'mlchoins',
+        streamerName: '명훈',
+        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/571589a1-c1bd-4396-9177-8be8bfd1f646-profile_image-70x70.png',
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '334',
+      },
+      {
+        streamerId: 'xkwhd',
+        streamerName: '피닉스박1',
+        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/00d773d6-0c5c-443a-947a-f3d765b95b4e-profile_image-70x70.jpg',
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '1,045',
+      },
+      {
+        streamerId: 'ayatsunoyuni_stellive',
+        streamerName: '아야츠노_유니',
+        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/bcd45a9c-d209-42d3-aa59-c546b6155891-profile_image-70x70.png',
+        game: 'Just Chatting',
+        isLive: '방송 중',
+        viewers: '580',
+      },
+      {
+        streamerId: 'shirayukihina_stellive',
+        streamerName: '시라유키_히나',
+        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/2c7b236c-2586-48dc-ba29-18e966ab2a48-profile_image-70x70.png',
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '333',
+      },
+      {
+        streamerId: 'nenekomashiro_stellive',
+        streamerName: '네네코_마시로',
+        profileImage: 'https://static-cdn.jtvnw.net/jtv_user_pictures/99903ff6-40c3-489c-b449-f8e338c716da-profile_image-70x70.png',
+        game: '마인크래프트',
+        isLive: '방송 중',
+        viewers: '573',
+      },
     ]
     useEffect(() => {
         const fetchData = async () => {
@@ -117,7 +177,7 @@ export const SideBar = ({serverSideData}: any) => {
                         }
                         <div className="relative InjectLayout-sc-1i43xsx-0 hWukFy tw-transition-group">
                           {streamerInfo.map((info) => (
-                            <StreamerProfileButton key={info.streamerName} info={info}/>
+                            <StreamerProfileButton key={info.streamerId} info={info} collapsed={extend}/>
                           ))}
                         </div>
                       </div>

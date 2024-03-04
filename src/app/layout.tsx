8994168bd49129ce0e01527658fc2a10335b0324
junghanlee <dlwjdgks126@hansung.ac.kr>
2024-03-04 15:43:56 +0900

    레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-    <body className="flex flex-col h-screen">
+    <body className="flex flex-col h-screen overflow-hidden">
     <div className="flex-0 w-full h-12">
       <Navbar/>
     </div>
@@ -28,12 +28,12 @@ export default function RootLayout({
       <div className="InjectLayout-sc-1i43xsx-0 iA-duFH twilight-main">
         <header id="twilight-sticky-header-root"></header>
         <div className="root-scrollable scrollable-area" data-a-target="root-scroller" data-simplebar="init">
-          <div className="simplebar-track vertical" style={{visibility: 'visible'}}>
-            <div className="simplebar-scrollbar" style={{top: '2px', height: '597px'}}></div>
-          </div>
-          <div className="simplebar-track horizontal" style={{visibility: 'hidden'}}>
-            <div className="simplebar-scrollbar" style={{left: '2px', width: '1055px'}}></div>
-          </div>
+          {/*<div className="simplebar-track vertical" style={{visibility: 'visible'}}>*/}
+          {/*  <div className="simplebar-scrollbar" style={{top: '2px', height: '597px'}}></div>*/}
+          {/*</div>*/}
+          {/*<div className="simplebar-track horizontal" style={{visibility: 'hidden'}}>*/}
+          {/*  <div className="simplebar-scrollbar" style={{left: '2px', width: '1055px'}}></div>*/}
+          {/*</div>*/}
           <div className="simplebar-scroll-content" style={{paddingRight: '22px', marginBottom: '-44px'}}>
             <div className="simplebar-content home__scrollable-content"
                  style={{paddingBottom: '22px', marginRight: '-22px'}}>

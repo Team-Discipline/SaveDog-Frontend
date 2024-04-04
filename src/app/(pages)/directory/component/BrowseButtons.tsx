cb30c32c2f0e9efc02bb1fb22decb6a0fd672240
gka0903 <ham3252@naver.com>
2024-04-04 19:19:42 +0900

    directory_all 속성

--- src/app/(pages)/directory/component/BrowseButtons.tsx
+++ src/app/(pages)/directory/component/BrowseButtons.tsx
@@ -10,7 +10,7 @@ interface ButtonItem {
 
 const BrowseButtons: React.FC = () => {
     return (
-        <div className="grid grid-cols-5 gap-3">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {ButtonList.map(({ buttonItem, buttonImg, category }: ButtonItem, index: number) => (
                 <BrowseButton
                     key={index}
